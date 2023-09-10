c850097518e6a1a29ce0cdfc67c7951f6c0082be
Victor Fernando Alves <97064368+vfalves@users.noreply.github.com>
2023-09-10 20:58:46 -0300

    Update script.js
    Code ongoing

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function isPalindrome(str){
     /* Seu código aqui */
+    let palavra = prompt('Informe a palavra ou frase: ')
+    // palavra = 'victor'
+    let palindromo = newArray()
+    
+    for(contador=0; contador < palavra.length; contador++){
+        palindromo.unshift() = palavra[contador]
+    }
+
+    if(palavra.toLowerCase()==palindromo.toLowerCase()){
+        alert(true)
+    } else{
+        alert(false)
+    }
 }
 
 function arrayMaxMin(arr){
