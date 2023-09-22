ef73803217e6d05592b086c2623761c699ed1982
Victor Fernando Alves <97064368+vfalves@users.noreply.github.com>
2023-09-22 18:26:46 -0300

    Tarefa realizada
    Códigos e testes realizados com sucesso.

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-function isPalindrome(str){
-    /* Seu código aqui */
-    let palavra = prompt('Informe a palavra ou frase: ')
-    
-    let palavra_invertida = palavra.split("").reverse().join("")
-    
-    if(palavra.toLowerCase()==palavra_invertida.toLowerCase()){
-        alert(true)
-    } else{
-        alert(false)
+function isPalindrome(palavraFrase) {
+
+    // Remova espaços em branco e converta para letras minúsculas
+    const texto = palavraFrase.replace(/\s/g, '').toLowerCase()
+    const tamanho = texto.length
+  
+    for (let i = 0; i < tamanho; i++) {
+        if (texto[i] !== texto[tamanho - 1 - i]) {
+            return false // Se encontrar um caractere diferente, não é um palíndromo.
+        }
     }
+  
+    // Se o loop terminar sem retornar false, é um palíndromo.
+    return true 
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+
+    // Inicializa min com o primeiro item do array
+    let minimo = arr[0] 
+
+    // Inicializa max com o primeiro item do array
+    let maximo = arr[0] 
+
+    for (let i = 1; i < arr.length; i++) {
+        // Atualiza o valor mínimo se encontrarmos um valor menor
+        if (arr[i] < minimo) minimo = arr[i]
+
+        // Atualiza o valor máximo se encontrarmos um valor maior
+        if (arr[i] > maximo) maximo = arr[i]
+    }
+
+    // Retorna um novo array com os valores mínimo e máximo
+    return [minimo, maximo] 
 }
